43c2e2ab027cb3a0f49ed78977c8008ce0e5419d
MelentQ <mejleht@yandex.ru>
2021-11-10 11:51:43 +0300

    Updates

--- assets/js/backend.js
+++ assets/js/backend.js
@@ -1,3 +1,44 @@
 document.addEventListener('DOMContentLoaded', () => {
-  
+  handleSubmit({
+    containerSelector: ".form-question",
+    formSelector: ".form",
+    submitBlockSelector: ".form__success-block",
+    openFormBtnSelector: ".form__success-block-again-btn"
+  }, {
+    hiddenFormClass: "send",
+    openedSuccessBlockClass: "visible"
+  })
 });
+
+/**
+ * Обработчик формы
+ * @param {String} containerSelector - CSS-селектор общего контейнера
+ * @param {String} formSelector - CSS-селектор формы
+ * @param {String} submitBlockSelector - CSS-селектор блока с оповещением об успешной отправке
+ * @param {String} openFormBtnSelector - CSS-селектор блока с оповещением об успешной отправке
+ * @param {String} hiddenFormClass - CSS-класс для скрытия формы
+ * @param {String} openedSuccessBlockClass - CSS-класс для показа блока с оповещением об успешной отправке
+ */
+function handleSubmit({containerSelector, formSelector, submitBlockSelector, openFormBtnSelector}, {hiddenFormClass, openedSuccessBlockClass}) {
+  const formContainer = document.querySelector(containerSelector);
+  if (!formContainer) return;
+
+  const form = formContainer.querySelector(formSelector);
+  const successBlock = formContainer.querySelector(submitBlockSelector);
+  const openFormBtn = successBlock.querySelector(openFormBtnSelector);
+
+  // Так же нужно дизейблить кнопку отправки формы при ожидании ответа от сервера
+  // Класс для дизейбла - disabled
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    form.classList.add(hiddenFormClass);
+    successBlock.classList.add(openedSuccessBlockClass);
+  })
+
+  openFormBtn.addEventListener('click', () => {
+    form.classList.remove(hiddenFormClass);
+    successBlock.classList.remove(openedSuccessBlockClass);
+  })
+}
